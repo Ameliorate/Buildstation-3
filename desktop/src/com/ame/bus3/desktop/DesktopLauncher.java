027876df25b0779285173fdee11c9882bcd1cfc1
Ameliorate <ameliorate2@gmail.com>
2015-03-04 20:40:23 -0500

    Adds libgdx and code

---
+++ desktop/src/com/ame/bus3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ame.bus3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ame.bus3.Buildstation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Buildstation(), config);
+	}
+}
