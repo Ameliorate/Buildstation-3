454ad951515edb6fda2a2d41bfcc021303a54497
Ameliorate <ameliorate2@gmail.com>
2015-03-07 14:13:01 -0500

    Fully implements a connection type

--- core/src/com/ame/bus3/common/Connection.java
+++ core/src/com/ame/bus3/common/Connection.java
@@ -1,8 +1,11 @@
 package com.ame.bus3.common;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
-import com.badlogic.gdx.utils.JsonValue;
+import org.json.simple.JSONArray;
 
 public class Connection implements Runnable {
 	
@@ -11,15 +14,60 @@ public class Connection implements Runnable {
 
 	@Override
 	public void run() {
-		if (client == null)
-			throw new IllegalArgumentException("No client was given");
+		if (connection == null)
+			throw new IllegalArgumentException("No connection was given");
 		else {
+			DataInputStream connectionGet = null;
+			try {
+				connectionGet = new DataInputStream(connection.getInputStream());
+			} 
+			catch (IOException e) {}
+			String data;
 			
+			while (true) {
+				try {
+					data = connectionGet.readUTF();
+				} 
+				catch (IOException e) {
+					disconnect();
+					break;	// The client has disconnected, so end the loop.
+				}
+				PacketSorterTracker.sort(data);
+			}
 		}
-			
 	}
 	
-	public void send(JsonValue packet) {
+	/**
+	 * Disconnects and cleans up stuff. You still will need to clean up any references to this class though.
+	 */
+	public void disconnect() {
+		try {
+			connection.close();
+		} 
+		catch (IOException e) {}
 		
+		connection = null;	// I'm not sure if this is necessary, but it can't do any harm.
+		thread = null;
+		System.out.println("[Info] Connection " + connection.toString() + " closed.");
+	}
+	
+	/**
+	 * 
+	 * @param packet
+	 */
+	public void send(JSONArray packet) {
+		DataOutputStream connectionOut = null;
+		try {
+			connectionOut = new DataOutputStream(connection.getOutputStream());
+		} 
+		catch (IOException e) {}
+		String data = packet.toJSONString();
+		
+		try {
+			connectionOut.writeUTF(data);
+		} 
+		catch (IOException e) {
+			disconnect();
+		}
 	}
 }
