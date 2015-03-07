0215fa6ad4b73d2b48ccd095f423628a0bd78b06
Ameliorate <ameliorate2@gmail.com>
2015-03-06 21:08:26 -0500

    Forgot part of a commit
    Commit 2c8b7b292e0da6c131c212668cc5779bc36b6107 to be exact.

---
+++ core/src/com/ame/bus3/common/Connection.java
@@ -0,0 +1,25 @@
+package com.ame.bus3.common;
+
+import java.net.Socket;
+
+import com.badlogic.gdx.utils.JsonValue;
+
+public class Connection implements Runnable {
+	
+	public Socket client;
+	public Thread thread;
+
+	@Override
+	public void run() {
+		if (client == null)
+			throw new IllegalArgumentException("No client was given");
+		else {
+			
+		}
+			
+	}
+	
+	public void send(JsonValue packet) {
+		
+	}
+}
