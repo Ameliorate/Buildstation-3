f0193c7d84afb22968a395c70143421379072d97
Ameliorate <ameliorate2@gmail.com>
2015-03-07 15:30:05 -0500

    Puts together all parts and makes the server work

--- core/src/com/ame/bus3/server/BuildstationServerMain.java
+++ core/src/com/ame/bus3/server/BuildstationServerMain.java
@@ -2,9 +2,22 @@ package com.ame.bus3.server;
 
 import com.ame.bus3.common.Variables;
 
+/**
+ * Main class for the server. Handles initalisation.
+ * @author Grant
+ *
+ */
 public class BuildstationServerMain {
+	/**
+	 * Starts the server.
+	 */
 	public void create() {
 		Variables.isServer = true;
-		System.out.println("It's Working! The Thing! It's Working!");
+		Variables.port = 25566;
+		ConnectionAccepter accepter = new ConnectionAccepter();
+		Thread accepterThread = new Thread(accepter);
+		accepterThread.start();
+		
+		System.out.println("[Info] Finished initalising");
 	}
 }
