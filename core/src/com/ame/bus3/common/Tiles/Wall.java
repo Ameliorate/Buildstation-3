32a07610be40aa15302136a92122402fe593b707
Ameliorate <ameliorate2@gmail.com>
2015-03-16 18:10:19 -0400

    Just a simple wall. Not much to look at.
    Completes #21

---
+++ core/src/com/ame/bus3/common/Tiles/Wall.java
@@ -0,0 +1,29 @@
+package com.ame.bus3.common.Tiles;
+
+import com.ame.bus3.client.TileTextureControler;
+import com.ame.bus3.common.BaseTile;
+import com.ame.bus3.common.Coordinate;
+import com.ame.bus3.common.Variables;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * A simple wall.
+ * @author Amelorate
+ */
+public class Wall extends BaseTile {
+	public Wall() {
+		if (Variables.isServer == false)
+			TileTextureControler.register("Wall", new Texture("Wall.png"));
+		spriteState.texture = "Wall";
+	}
+
+	@Override
+	public void clone(Coordinate location) {
+		Variables.map.place(new Wall(), location);
+	}
+
+	@Override
+	public String getType() {
+		return "Wall";
+	}
+}
