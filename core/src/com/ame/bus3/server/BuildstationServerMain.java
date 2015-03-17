63b4d1dd3a560f1843931538299a9bf6e01d3374
Ameliorate <ameliorate2@gmail.com>
2015-03-17 17:06:05 -0400

    More of the last commit
    ee2e67da150d8cef6306603e6fb3578a748cc293

--- core/src/com/ame/bus3/server/BuildstationServerMain.java
+++ core/src/com/ame/bus3/server/BuildstationServerMain.java
@@ -1,6 +1,9 @@
 package com.ame.bus3.server;
 
+import com.ame.bus3.common.Coordinate;
+import com.ame.bus3.common.Tile;
 import com.ame.bus3.common.TileRegisterer;
+import com.ame.bus3.common.Tiles.Wall;
 import com.ame.bus3.common.Variables;
 import com.ame.bus3.common.packetsorters.SorterList;
 
@@ -17,6 +20,8 @@ public class BuildstationServerMain {
 		Variables.isServer = true;	// Make sure this is done first.
 
 		TileRegisterer.load();		// Registerers tiles.
+
+		Variables.map.fill(new Coordinate(0, 0, 0, "default"), new Coordinate(15, 15, 0, "default"), new Wall());
 
 		SorterList.loadSorters();		// Begin networking
 		Variables.port = 25566;
