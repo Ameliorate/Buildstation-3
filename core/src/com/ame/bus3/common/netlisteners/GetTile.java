ac89da5983b9950523098d89b84d6350a43959ab
Ameliorate <ameliorate2@gmail.com>
2015-04-16 21:37:45 -0400

    GetTile listener logic

--- core/src/com/ame/bus3/common/netlisteners/GetTile.java
+++ core/src/com/ame/bus3/common/netlisteners/GetTile.java
@@ -1,6 +1,8 @@
 package com.ame.bus3.common.netlisteners;
 
 import com.ame.bus3.common.Coordinate;
+import com.ame.bus3.common.Tile;
+import com.ame.bus3.common.Variables;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
@@ -12,11 +14,26 @@ public class GetTile extends Listener {
 
 	@Override
 	public void received(Connection connection, Object object) {
+		if (object instanceof GetTilePacket) {
+			Tile tile = Variables.map.get((Coordinate) object);
+			PlaceTile.send(tile, connection);
+		}
 	}
 
 	/**
 	 * Gets a tile from the server.
 	 */
 	public static void send(Coordinate location, Connection connection) {
+		connection.sendTCP(new GetTilePacket(location));
+	}
+
+	private static class GetTilePacket {
+		public GetTilePacket() {}
+
+		public GetTilePacket(Coordinate location) {
+			this.location = location;
+		}
+
+		public Coordinate location;
 	}
 }
