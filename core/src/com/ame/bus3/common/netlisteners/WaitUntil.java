d36aa27e1c768d6046fe281ede3b1c3892eb4d19
Ameliorate <ameliorate2@gmail.com>
2015-04-16 21:10:35 -0400

    Adds wait until listener

--- core/src/com/ame/bus3/common/netlisteners/WaitUntil.java
+++ core/src/com/ame/bus3/common/netlisteners/WaitUntil.java
@@ -2,10 +2,8 @@ package com.ame.bus3.common.netlisteners;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
-import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Allows waiting until a connection says it has finished a task.
@@ -22,13 +20,32 @@ public class WaitUntil extends Listener {
 	 * Sends the specified packet.
 	 * @param finished What you have finished doing.
 	 */
-	@SuppressWarnings("unchecked")
 	public static void send(Connection sending, String finished) {
+		sending.sendTCP(new WaitUntilPacket(finished));
 	}
 
 	/**
 	 * Waits for a certain task to complete.
 	 */
 	public static void wait(String waitingCondition) {
+		int oldPriority = Thread.currentThread().getPriority();
+		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);	// We don't want to be clogging up the cpu with an empty loop.
+
+		//noinspection StatementWithEmptyBody
+		while (!finished.contains(waitingCondition)) {}
+
+		Thread.currentThread().setPriority(oldPriority);
 	}
+
+	private static class WaitUntilPacket {
+		public WaitUntilPacket() {
+		}
+
+		public WaitUntilPacket(String condition) {
+			this.condition = condition;
+		}
+
+		public String condition;
+	}
+
 }
