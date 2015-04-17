25704b62a0c589eb29b9ef7fbcc31cdace292c43
Ameliorate <ameliorate2@gmail.com>
2015-04-16 21:26:26 -0400

    Terminal display listener logic

--- core/src/com/ame/bus3/common/netlisteners/TerminalDisplay.java
+++ core/src/com/ame/bus3/common/netlisteners/TerminalDisplay.java
@@ -11,8 +11,21 @@ import com.esotericsoftware.kryonet.Listener;
 public class TerminalDisplay extends Listener {
 	@Override
 	public void received(Connection connection, Object object) {
+		if (object instanceof TerminalDisplayPacket)
+			System.out.println(((TerminalDisplayPacket) object).message);
 	}
 
 	public static void send(String message, Connection connection) {
+		connection.sendTCP(new TerminalDisplayPacket(message));
+	}
+
+	private static class TerminalDisplayPacket {
+		public TerminalDisplayPacket() {}
+
+		public TerminalDisplayPacket(String message) {
+			this.message = message;
+		}
+
+		public String message;
 	}
 }
