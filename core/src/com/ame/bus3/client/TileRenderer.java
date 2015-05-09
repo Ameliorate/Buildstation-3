09b23edeb9234f58e7d221758fb2153b05c0cb11
Ameliorate <ameliorate2@gmail.com>
2015-05-09 18:14:33 -0400

    Removes the current tile renderer implementation
    I need to redo this anyways.

--- core/src/com/ame/bus3/client/TileRenderer.java
+++ core/src/com/ame/bus3/client/TileRenderer.java
@@ -4,6 +4,7 @@ import com.ame.bus3.common.*;
 import com.ame.bus3.common.Tiles.Wall;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.HashMap;
 
@@ -14,8 +15,6 @@ import java.util.HashMap;
 public class TileRenderer implements Renderer {
 	@SuppressWarnings({"WeakerAccess", "CanBeFinal"})
 	public Coordinate[] renderLayers = {new Coordinate(0, 0, 0, "default")};
-	@SuppressWarnings({"CanBeFinal", "MismatchedQueryAndUpdateOfCollection"})
-	private HashMap<ChunkCacheKey, Chunk> chunkCache = new HashMap<>();
 	private static final int PIXELS_PER_TILE = 48;
 
 	public TileRenderer() {
@@ -24,67 +23,6 @@ public class TileRenderer implements Renderer {
 
 	@Override
 	public void render(SpriteBatch batch, World world) {
-		Texture drawing;
-		Tile drawingTile = new Wall();	// I initialise this with wall so that the loop works correctly.
-		SpriteState state;
-		int tileXPixel;
-		int tileYPixel;
-
-		for(Coordinate currentLayer : renderLayers) {
-			for (int x = -1; x <= 1; x++)
-				for (int y = -1; y <= 1; y++) {
-					int xChunkPos = (currentLayer.getX() / 16) + x;
-					int yChunkPos = (currentLayer.getY() / 16) + y;
-
-					Chunk cacheingChunk = world.map.getChunk(new Coordinate(xChunkPos, yChunkPos, 0, currentLayer.getLevel()));
-					ChunkCacheKey key = new ChunkCacheKey(x, y, currentLayer.getLevel());
-					chunkCache.put(key, cacheingChunk);
-				}
-
-			for (int x = currentLayer.getX(); x <= 15; x++)
-				for (int y = currentLayer.getY(); y <= 15; y++)
-					for (int z = 0; drawingTile != null; z++) {
-						System.out.println("Render try");
-						drawingTile = world.map.get(new Coordinate(x, y, z, currentLayer.getLevel()));
-						System.out.println("boo");
-						if (drawingTile == null) {
-							System.out.println("null thingy");
-							break;
-						} else {
-							state = drawingTile.renderTick();
-							tileXPixel = drawingTile.getPosition().getX() * PIXELS_PER_TILE;
-							tileYPixel = drawingTile.getPosition().getY() * PIXELS_PER_TILE;
-							drawing = TileTextureRegistry.get(state.texture);
-
-							batch.draw(drawing, tileXPixel, tileYPixel, 24, 24, 48, 48, 1, 1, state.rotation, 0, 0, 48, 48, state.flipX, state.flipY);
-							/*
-							It's probably better to rewrite the above statement if you want to maintain this.
-							This will help:
-							https://stackoverflow.com/questions/24748350/libgdx-rotate-a-texture-when-drawing-it-with-spritebatch
-							 */
-							System.out.println("Render!");
-						}
-					}
-		}
-	}
-
-	private class ChunkCacheKey {
-		public ChunkCacheKey(int x, int y, String level) {
-			this.x = x;
-			this.y = y;
-			this.level = level;
-		}
-
-		@SuppressWarnings("CanBeFinal")
-		public int x;
-		@SuppressWarnings("CanBeFinal")
-		public int y;
-		@SuppressWarnings("CanBeFinal")
-		public String level;
-
-		@Override
-		public int hashCode() {
-			return x + y + level.hashCode();
-		}
+		throw new NotImplementedException();
 	}
 }
