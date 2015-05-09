469d044cb27632d954b80699b084cde6ab41415e
Ameliorate <ameliorate2@gmail.com>
2015-05-08 22:12:29 -0400

    Fixes an issue with waituntill now waiting
    Aparently the empty loop was optimised out.

--- core/src/com/ame/bus3/common/netlisteners/WaitUntil.java
+++ core/src/com/ame/bus3/common/netlisteners/WaitUntil.java
@@ -13,11 +13,16 @@ import java.util.HashMap;
 public class WaitUntil extends Listener {
 	@SuppressWarnings("CanBeFinal")
 	private static HashMap<String, Boolean> finished = new HashMap<>();	// Find a better way to do this.
+	private static final Object lock = new Object();
 
 	@Override
 	public void received(Connection connection, Object object) {
-		if (object instanceof WaitUntilPacket)
+		if (object instanceof WaitUntilPacket) {
 			finished.put(((WaitUntilPacket) object).condition, true);
+			synchronized (lock) {
+				lock.notify();
+			}
+		}
 	}
 
 	/**
@@ -32,14 +37,18 @@ public class WaitUntil extends Listener {
 	 * Waits for a certain task to complete. Not thread safe with the same keys.
 	 */
 	public static void wait(String waitingCondition) {
-		int oldPriority = Thread.currentThread().getPriority();
-		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);	// We don't want to be clogging up the cpu with an empty loop.
-		finished.put(waitingCondition, false);
-
-		//noinspection StatementWithEmptyBody
-		while (!finished.get(waitingCondition)) {}
-
-		Thread.currentThread().setPriority(oldPriority);
+		synchronized (lock) {
+			while (true) {
+				try {
+					lock.wait();
+				}
+				catch (InterruptedException e) {
+					if (finished.get(waitingCondition)) {
+						break;
+					}
+				}
+			}
+		}
 	}
 
 	private static class WaitUntilPacket {
